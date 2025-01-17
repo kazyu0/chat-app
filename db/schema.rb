44a0fb7c15676d38c296a06a04e8bfdc25a8ef6d
kazyu0 <kazumakimijima.sb@gmail.com>
2025-01-17 14:45:09 +0900

    「room_usersテーブルの作成」

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_17_032001) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_17_053945) do
+  create_table "room_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rooms", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
